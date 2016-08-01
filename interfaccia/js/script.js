78887fbe0b2c4c657d7dc1049ed34a2666f1f997
Salvatore Piras <Salvatore Piras>
2016-08-01 11:16:20 +0200

    script della pagina riepilogo

--- interfaccia/js/script.js
+++ interfaccia/js/script.js
@@ -1,41 +1,48 @@
-/*
 
-function menu(){
-  var elementoMenu=$("<menu></menu");
-//<button type="button" name="Rinomina" id="RINOMINA">RINOMINA</button>
-//<button type="button" name="Visualizza" id="VISUALIZZA">VISUALIZZA</button>
-//<button type="button" name="Cancella" id="CANCELLA">CANCELLA</button>
-  var buttonMenu1 = $("<button></button>");
-  $(buttonMenu1).attr("type", "button");
-  $(buttonMenu1).attr("name", "Rinomina");
-  $(buttonMenu1).attr("id", "RINOMINA");
-  buttonMenu1.text("RINOMINA");
-  $(elementoMenu).append($(buttonMenu1));
-
-var buttonMenu2 = $("<button></button>");
-  $(buttonMenu2).attr("type", "button");
-  $(buttonMenu2).attr("name", "Visualizza");
-  $(buttonMenu2).attr("id", "VISUALIZZA");
-  buttonMenu2.text("VISUALIZZA");
-  $(elementoMenu).append($(buttonMenu2));
-
-var buttonMenu3 = $("<button></button>");
-  $(buttonMenu3).attr("type", "button");
-  $(buttonMenu3).attr("name", "Cancella");
-  $(buttonMenu3).attr("id", "CANCELLA");
-  buttonMenu3.text("CANCELLA");
-  $(elementoMenu).append($(buttonMenu3));
+function inserimentoElenco(data){
+  var elementoMenu=$("<option></option>");
 
 
-  $("#name").append(elementoMenu);
+  $(elementoMenu).attr("value", data.nome);
+  $(elementoMenu).text(data.nome);
+
+
+  $("#elenco").append(elementoMenu);
 
 }
+/*
+function creaElenco(){
+  j.ajax({
+    url: "../server/Elenco.php",
+    type: "POST",
+    data:{nome:nome},
+    datatype:"json",
+    success:
+      function(){
+        foreach(data as data){
+          inserimentoElenco(data);
+        }
+      },
+    error:
+      console.log("errore nell'interrogazione della pagina Elenco")
+  })
+}
 */
-
 $(document).ready(function(){
-  
+
 
   $("#RINOMINA").click(function(){
+    var nome=$("<input>");
+    $(nome).attr("type","text");
+    $(nome).attr("id","RINAME");
+
+    var pulsante=$("<button></button>");
+    $(pulsante).attr("type","button");
+    $(pulsante).attr("id","conferma");
+    $(pulsante).text("CONFERMA");
+    $("#name").append(nome);
+    $("#name").append(pulsante);
+
   });
 
   $("#VISUALIZZA").click(function(){
--- interfaccia/server/Elenco.php
+++ interfaccia/server/Elenco.php
@@ -1,12 +1,23 @@
 <?php
 
-$fileElenco =fopen("indirizzo","r+");
-
-$ArrayJson =array();
-
-foreach ($ArrayJson as $key => $value) {
-  # code...
+function converti($array){
+  $array_json = array();
+  foreach($array as $elemento){
+    $decode = json_decode($elemento);
+    array_push ($array_json, $decode);
+  }
+  return $array_json;
 }
+
+
+  $fileElenco =fopen("ElencoMesh","r+");
+
+  $risult=fgets($fileElenco);
+  $array= explode(';',$result);
+	$elementi_js = converti($array);
+
+  echo json_encode($elementi_js);
+
 
 
 
